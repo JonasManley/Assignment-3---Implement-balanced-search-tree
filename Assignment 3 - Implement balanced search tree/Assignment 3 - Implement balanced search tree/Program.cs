80e46cddaddff7490fcc23205e694f09719c587c
JonasManley <54889189+JonasManley@users.noreply.github.com>
2020-03-24 09:38:14 +0100

    Project added
    Containing the algorithm

---
+++ Assignment 3 - Implement balanced search tree/Assignment 3 - Implement balanced search tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3___Implement_balanced_search_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVL tree = new AVL();
+            tree.Add(5);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(3);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(7);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(2);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(22);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(12);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Add(9);
+            tree.DisplayTree(); // for visualization purposes
+            tree.Delete(7);
+            tree.DisplayTree(); // for visualization purposes
+
+            Console.ReadLine();
+        }
+    }
+}
